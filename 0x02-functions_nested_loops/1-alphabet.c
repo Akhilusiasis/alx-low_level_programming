4ef0c4eb440c66262d9939504bd8a1fc896b279e
Akhilusiasis <akispeed123@gmail.com>
2022-07-05 15:19:51 -0700

    alphabet order

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+
+ * main - check the code
+
+ *
+
+ * Return: Always 0.
+
+ */
+void print_alphabet(void);
+void print_alphabet(void)
+{
+  char letter = 'a';
+  while (letter <= 'z')
+    {
+      _putchar(letter);
+    }
+  _putchar('\n');
+}
+int main(void)
+{
+  print_alphabet();
+  return (0);
+}
