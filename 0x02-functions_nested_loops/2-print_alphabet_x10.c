edc5bfa90945d054c2c86409bc0248661c6afd2e
Akhilusiasis <akispeed123@gmail.com>
2022-07-05 17:30:05 -0700

    isLower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
- 
-  char alpha; 
+
+  
+  char alpha;
+  
   int counter;
+  
   for (counter = 1; counter <= 10; counter++)
+
     
     {
       for (alpha = 'a'; alpha <= 'z'; alpha++)
+
 	
 	{
 	  _putchar(alpha); 
