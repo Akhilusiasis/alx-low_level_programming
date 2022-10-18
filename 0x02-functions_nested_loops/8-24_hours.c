4eab2c0daf42cc287862933102080d7944cd0ef3
Aklilu H <92521206+Akhilusiasis@users.noreply.github.com>
2022-10-18 09:51:39 -0900

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,19 +8,19 @@ void jack_bauer(void)
 {
 int hours = 0;
 int min = 0;  
-while (hours < 24)    
-{      
-while (min < 60)	
+while (hours < 24)
 {
-_putchar('0' + (hours / 10));	  
+while (min < 60)
+{
+_putchar('0' + (hours / 10));
 _putchar('0' + (hours % 10));
-_putchar(':');	  
-_putchar('0' + (min / 10));	  
-_putchar('0' + (min % 10));	  
-_putchar('\n');	  
-min++;	  
-}      
-min = 0;      
-hours++;      
-}  
+_putchar(':');
+_putchar('0' + (min / 10));
+_putchar('0' + (min % 10));
+_putchar('\n');
+min++;
+}
+min = 0;
+hours++;
+}
 }
